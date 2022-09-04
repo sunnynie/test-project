86c870ca33ae9ea86ab73dc8e93028a4ce28596f
nie_chaoqun <1922692221@qq.com>
2022-09-04 16:54:18 +0800

    add mybatis2cache

---
+++ mybatis2cache/src/main/java/com/nie/mybatis2cache/Mybatis2CacheApplication.java
@@ -0,0 +1,12 @@
+package com.nie.mybatis2cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mybatis2CacheApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Mybatis2CacheApplication.class, args);
+        System.out.println("mybatis2cache 启动成功");
+    }
+}
